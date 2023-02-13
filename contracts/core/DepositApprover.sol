4df2ceb35085f57e412a0938aa33716b3de97155
unknown <kraiglyons0929@gmail.com>
2023-02-13 21:31:04 +0800

    add whitelist to deposit approver

--- contracts/core/DepositApprover.sol
+++ contracts/core/DepositApprover.sol
@@ -4,26 +4,35 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import "../interfaces/IWhitelist.sol";
 import "../interfaces/IVault.sol";
 
 contract DepositApprover is Ownable {
     address public vault;
     address public asset;
+    address public whiteList;
 
     event SetVault(address vault);
+
+    event SetWhitelist(address whiteList);
 
     constructor(address _asset) {
         asset = _asset;
     }
 
-    function deposit(uint256 amount) public {
+    modifier onlyAllowed() {
+        require(tx.origin == msg.sender || IWhitelist(whiteList).listed(msg.sender), "NON_LISTED_CA");
+        _;
+    }
+
+    function deposit(uint256 amount, uint256 receiver) public onlyAllowed {
         require(getBalance(msg.sender) >= amount, "INSUFFICIENT_AMOUNT");
         require(IERC20(asset).allowance(msg.sender, address(this)) >= amount, "INSUFFICIENT_ALLOWANCE");
 
         uint256 prevBal = getBalance(address(vault));
         IERC20(asset).transferFrom(msg.sender, vault, amount);
         uint256 newBal = getBalance(address(vault));
-        IVault(vault).deposit(newBal - prevBal, msg.sender);
+        IVault(vault).deposit(newBal - prevBal, receiver);
     }
 
     function getBalance(address account) internal view returns (uint256) {
@@ -38,5 +47,12 @@ contract DepositApprover is Ownable {
         vault = _vault;
 
         emit SetVault(vault);
+    }
+
+    function setWhitelist(address _whitelist) public onlyOwner {
+        require(_whitelist != address(0), "INVALID_ZERO_ADDRESS");
+        whiteList = _whitelist;
+
+        emit SetWhitelist(whiteList);
     }
 }
